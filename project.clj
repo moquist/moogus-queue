89db9b7f593784a73eeca5ea7561ab5515579879
Matt Oquist <moquist@majen.net>
2014-06-10 17:45:51 -0400

    bump clj-http version

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.json "0.2.4"]
-                 [clj-http "0.9.1"]
+                 [clj-http "0.9.2"]
                  [liberator "0.11.0"]
                  [com.datomic/datomic-free "0.9.4766"
                   :exclusions [org.jboss.logging/jboss-logging org.jgroups/jgroups]
