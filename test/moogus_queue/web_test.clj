d1841fe055e9df9de1edaf6ca304ec67c8d411ba
Matt Oquist <moquist@majen.net>
2014-06-24 16:38:28 -0400

    initial test.check experiment

--- test/moogus_queue/web_test.clj
+++ test/moogus_queue/web_test.clj
@@ -3,9 +3,14 @@
             [immutant.dev]
             [immutant.web]
             [datomic.api :as d]
+            [clojure.test.check :as tc]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]
             [moogus-queue]
             [moogus-queue.web]
             [moogus-queue.testlib :as mqt]))
+
+(def num-quick-checks 1000)
 
 (defn testing-fixture [f]
   (alter-var-root #'moogus-queue/system moogus-queue/start-system!)
@@ -14,15 +19,18 @@
 
 (use-fixtures :once testing-fixture)
 
-(deftest web-assert-queue-entry
+(deftest web-assert-queue-entry-test
   (let [db-conn (:db-conn moogus-queue/system)
-        data "womp-a-domp"
-        t (moogus-queue.web/assert-queue-entry db-conn data)]
-    (testing "assert-queue-entry"
-      (is (mqt/ensure-tx t))
-      (is (integer? (ffirst
-                     (d/q '[:find ?e
-                            :in $ ?data
-                            :where [?e :queue-entry/message ?data]]
-                          (d/db db-conn)
-                          data)))))))
+        prop (prop/for-all
+              [msg gen/string]
+              (let [t (moogus-queue.web/assert-queue-entry db-conn msg)]
+                (testing "assert-queue-entry"
+                  (is (mqt/ensure-tx t))
+                  (is (integer? (ffirst
+                                 (d/q '[:find ?e
+                                        :in $ ?data
+                                        :where [?e :queue-entry/message ?data]]
+                                      (d/db db-conn)
+                                      msg)))))))
+        t (tc/quick-check num-quick-checks prop)]
+    (is (:result t) (str t))))
